6c36b60e2261f203abbb9b9cd566dd21b25b1bda
sp <sampeppard@gmail.com>
2016-10-14 15:51:37 -0400

    add functionality to display multiple user repos when searched

--- js/user_lookup.js
+++ js/user_lookup.js
@@ -4,16 +4,37 @@ function User() {
 }
 
 User.prototype.getUsers =  function(username) {
+    var results = $(".results");
     $.get("https://api.github.com/users/" + username + "?access_token=" + apiKey).then(function(response) {
         console.log(response);
-        var results = $(".results");
         results.append(
-                        "<ul class='list-group'>" +
-                            "<li class='list-group-item'>Username: " + response.login + "</li>" +
-                            "<li class='list-group-item'><img width='233' height='233' src='" + response.avatar_url + "'></li>" +
-                            "<li class='list-group-item'>Location: " + response.location + "</li>" +
-                        "</ul>"
-                    );
+            "<ul class='list-group'>" +
+                "<li class='list-group-item'>Username: " + response.login + "</li>" +
+                "<li class='list-group-item'>Name: " + response.name + "</li>" +
+                "<li class='list-group-item'><img width='233' height='233' src='" + response.avatar_url + "'></li>" +
+                "<li class='list-group-item'>Location: " + response.location + "</li>" +
+            "</ul>"
+        );
+    }).fail(function(error) {
+        results.append("<h3>Sorry, we can't seem to find what your looking for :(</h3>");
+        console.log(error.responseJSON.message);
+    });
+};
+
+User.prototype.getRepos = function(username) {
+    var repos = $(".repos");
+    $.get("https://api.github.com/users/" + username + "/repos?access_token=" + apiKey).then(function(response) {
+        console.log(response.length);
+        repos.append("<h3>Repositories</h3>");
+        for (var i = 0; i < response.length; i++) {
+            repos.append(
+                "<ul class='list-group'>" +
+                    "<li class='list-group-item'>Name: " + response[i].name + "</li>" +
+                    "<li class='list-group-item'>Descrition: " + response[i].description + "</li>" +
+                    "<li class='list-group-item'>Link: <a href='" + response[i].clone_url + "'>" + response[i].clone_url + "</a></li>" +
+                "</ul>"
+            );
+        }
     }).fail(function(error) {
         console.log(error.responseJSON.message);
     });
--- js/user_lookup_interface.js
+++ js/user_lookup_interface.js
@@ -3,8 +3,11 @@ var User = require("./../js/user_lookup.js").userModule;
 $(document).ready(function() {
     var newUserSearch = new User();
     $("#user-lookup").submit(function(event) {
+        $(".results").html("");
+        $(".repos").html("");
         event.preventDefault();
         var username = $("#user").val();
         newUserSearch.getUsers(username);
+        newUserSearch.getRepos(username);
     });
 }); // end ready
