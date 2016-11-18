0e657f17229daf4797a5237eac1a95e25c29b70c
sp <sampeppard@gmail.com>
2016-11-17 19:07:02 -0500

    add missing gulp file dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -32,16 +32,16 @@ var lib = require('bower-files')({
 });
 
 gulp.task("bowerJS", function() {
-    return gulp.src(lib.ext("js").files)
-        .pipe(concat("vendor.min.js"))
-        .pipe(uglify())
-        .pipe(gulp.dest("build/js"));
+  return gulp.src(lib.ext("js").files)
+    .pipe(concat("vendor.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("build/js"));
 });
 
 gulp.task("bowerCSS", function() {
-    return gulp.src(lib.ext("css").files)
-        .pipe(concat("vendor.css"))
-        .pipe(gulp.dest("build/css"));
+  return gulp.src(lib.ext("css").files)
+    .pipe(concat("vendor.css"))
+    .pipe(gulp.dest("build/css"));
 });
 
 gulp.task("bower", ["bowerJS", "bowerCSS"]);
@@ -51,69 +51,69 @@ gulp.task("bower", ["bowerJS", "bowerCSS"]);
 //Javascript Components
 
 gulp.task("concatInterface", function() {
-    return gulp.src(["./js/*_interface.js"])
-        .pipe(concat("allConcat.js"))
-        .pipe(gulp.dest("./tmp"));
+  return gulp.src(["./js/*_interface.js"])
+    .pipe(concat("allConcat.js"))
+    .pipe(gulp.dest("./tmp"));
 });
 
 gulp.task("jsBrowserify", ["concatInterface"], function() {
-    return browserify({ entries: ["./tmp/allConcat.js"]})
-        .bundle()
-        .pipe(source("app.js"))
-        .pipe(gulp.dest("./build/js"));
+  return browserify({ entries: ["./tmp/allConcat.js"]})
+    .bundle()
+    .pipe(source("app.js"))
+    .pipe(gulp.dest("./build/js"));
 });
 
 gulp.task("minifyScripts", ["jsBrowserify"], function() {
-    return gulp.src("build/js/app.js")
-        .pipe(uglify())
-        .pipe(gulp.dest("build/js"));
+  return gulp.src("build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("build/js"));
 });
 
 gulp.task("clean", function() {
-    return del(["build", "tmp"]);
+  return del(["build", "tmp"]);
 });
 
 gulp.task("build", ["clean"], function() {
-    if (buildProduction) {
-        gulp.start("minifyScripts");
-    } else {
-        gulp.start("jsBrowserify");
-    }
-    gulp.start("bower");
+  if (buildProduction) {
+    gulp.start("minifyScripts");
+  } else {
+    gulp.start("jsBrowserify");
+  }
+  gulp.start("bower");
 });
 
 // Linter
 
 gulp.task("jshint", function() {
-    return gulp.src(["js/*.js"])
-        .pipe(jshint())
-    .pipe(jshint.reporter("default"));
+  return gulp.src(["js/*.js"])
+    .pipe(jshint())
+  .pipe(jshint.reporter("default"));
 });
 
 
 // Server For Live Reloading
 
 gulp.task("jsReload", ["jsBrowserify", "jshint"], function(){
-    browserSync.reload();
+  browserSync.reload();
 });
 
 gulp.task("bowerReload", ["bower"], function() {
-    browserSync.reload();
+  browserSync.reload();
 });
 
 gulp.task("htmlReload", function() {
-    browserSync.reload();
+  browserSync.reload();
 });
 
 
 gulp.task("serve", function() {
-    browserSync.init({
-        server: {
-            baseDir: "./",
-            index: "index.html"
-        }
-    });
-    gulp.watch(["js/*.js"], ["jsReload"]);
-    gulp.watch(["bower.json"], ["bowerReload"]);
-    gulp.watch(["*.html"], ["htmlReload"]);
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+  gulp.watch(["js/*.js"], ["jsReload"]);
+  gulp.watch(["bower.json"], ["bowerReload"]);
+  gulp.watch(["*.html"], ["htmlReload"]);
 });
--- js/user_lookup_interface.js
+++ js/user_lookup_interface.js
@@ -1,13 +1,13 @@
 var User = require("./../js/user_lookup.js").userModule;
 
 $(document).ready(function() {
-    var newUserSearch = new User();
-    $("#user-lookup").submit(function(event) {
-        $(".results").html("");
-        $(".repos").html("");
-        event.preventDefault();
-        var username = $("#user").val();
-        newUserSearch.getUsers(username);
-        newUserSearch.getRepos(username);
-    });
+  var newUserSearch = new User();
+  $("#user-lookup").submit(function(event) {
+    $(".results").html("");
+    $(".repos").html("");
+    event.preventDefault();
+    var username = $("#user").val();
+    newUserSearch.getUsers(username);
+    newUserSearch.getRepos(username);
+  });
 }); // end ready
